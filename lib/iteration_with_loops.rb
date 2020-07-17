5ad98f35180a626340da3255004ef537b689635a
Kelsey Creehan <kelseycreehan@gmail.com>
2020-07-17 22:11:04 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ while row_index < src.count do
   element_index = 0
   low = 0
   while element_index < src[row_index].count do
-    if src[row_index][element_index] > low
+    if src[row_index][element_index] < low
     low = src[row_index][element_index]
   end
     element_index += 1
